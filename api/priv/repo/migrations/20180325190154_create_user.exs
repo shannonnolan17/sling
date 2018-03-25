19ccf8ca7581f11a2c73c18ea64145b55b7d3efe
Shannon Nolan <shannonnolan17@gmail.com>
2018-03-25 13:07:30 -0700

    migrate users table

--- api/priv/repo/migrations/20180325190154_create_user.exs
+++ api/priv/repo/migrations/20180325190154_create_user.exs
@@ -3,11 +3,14 @@ defmodule Sling.Repo.Migrations.CreateUser do
 
   def change do
     create table(:users) do
-      add :username, :string
-      add :email, :string
-      add :password_hash, :string
+      add :username, :string, null: false
+      add :email, :string, null: false
+      add :password_hash, :string, null: false
 
       timestamps()
     end
+  create unique_index(:users, [:username])
+  create unique_index(:users, [:email])
   end
+
 end
