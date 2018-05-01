8acbe6f1a8a6a50f024d6694cb717d5167a4f91c
Shannon Nolan <shannonnolan17@gmail.com>
2018-04-30 18:38:44 -0700

    Allow phoenix to set up to accept http requests

--- api/mix.exs
+++ api/mix.exs
@@ -40,7 +40,8 @@ defmodule Sling.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:comeonin, "~> 2.5"},
-      {:guardian, "~> 1.0"}
+      {:guardian, "~> 1.0"},
+      {:cors_plug, "~> 1.1"}
     ]
   end
 
